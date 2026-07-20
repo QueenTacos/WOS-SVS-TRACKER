292ecc1fdbaadf952b5afdf7d7833fb7ead6a960
QueenTacos <yumqueentacos@gmail.com>
2026-07-19 22:23:35 -0400

    Delete src directory

--- src/components/EmptyState.jsx
+++
@@ -1,5 +0,0 @@
-import { MUTED } from '../lib/theme.js';
-
-export function EmptyState({icon,text}){
-  return(<div style={{textAlign:"center",padding:"52px 0",color:MUTED}}><div style={{fontSize:42,marginBottom:12}}>{icon}</div><p style={{fontSize:14}}>{text}</p></div>);
-}
